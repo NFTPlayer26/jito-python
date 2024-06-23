3efe778099530e059f3f51f2613e6d9222d96c8f
NFTPlayer26 <94644025+NFTPlayer26@users.noreply.github.com>
2024-06-23 14:34:30 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='pyhton_jito_bundle',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(where="."),  # include all packages in the project
+    package_dir={"": "."},  # root directory as the base
     install_requires=[
         'solana',
         'solders',
@@ -12,6 +13,5 @@ setup(
         'protobuf',
         'click',
         'asyncio',
-        # Diğer bağımlılıkları buraya ekleyin
     ],
 )
