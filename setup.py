f06d2e4c93f87427cabf2c00b0ec0d9a71cadf7f
NFTPlayer26 <94644025+NFTPlayer26@users.noreply.github.com>
2024-06-23 14:36:36 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='pyhton_jito_bundle',
     version='0.1',
-    packages=find_packages(where="."),  # include all packages in the project
-    package_dir={"": "."},  # root directory as the base
+    packages=find_packages(include=[
+        "jito_geyser", "jito_geyser.*",
+        "jito_searcher_client", "jito_searcher_client.*",
+        "mev-protos", "mev-protos.*"
+    ]),
     install_requires=[
         'solana',
         'solders',
