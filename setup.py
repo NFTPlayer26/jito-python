fc68d2664347bdab069cf0031aab0b5c0fe2fb3a
NFTPlayer26 <94644025+NFTPlayer26@users.noreply.github.com>
2024-06-23 14:44:13 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,18 @@ from setuptools import setup, find_packages
 setup(
     name='pyhton_jito_bundle',
     version='0.1',
-    packages=find_packages(include=[
-        "*", "*.*"
-    ]),
+    packages=find_packages(),
     install_requires=[
-        'solana', 'solders', 'spl', 'grpcio', 'protobuf', 'click', 'asyncio'
+        'solana',
+        'solders',
+        'spl',
+        'grpcio',
+        'protobuf',
+        'click',
+        # asyncio'nun burada olması gerekmiyor çünkü Python'ın standart kütüphanesinin bir parçası
     ],
+    package_data={
+        '': ['*.py', '*.pyi'],
+    },
+    include_package_data=True,
 )
