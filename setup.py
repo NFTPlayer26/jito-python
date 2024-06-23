e1df1748ceee3951f83c763e4951cc3746db95e7
NFTPlayer26 <94644025+NFTPlayer26@users.noreply.github.com>
2024-06-23 14:48:02 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name='pyhton_jito_bundle',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(include=[
+        'geyser_grpc_plugin',
+        'jito_geyser',
+        'jito_searcher_client',
+        'jito_searcher_client.*',
+        'mev_protos'
+    ]),
     install_requires=[
         'solana',
         'solders',
