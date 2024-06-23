d272fa011b4cd41805902576e1502c566ae3f7cf
NFTPlayer26 <94644025+NFTPlayer26@users.noreply.github.com>
2024-06-23 15:00:15 +0300

    Update __init__.py

--- jito_searcher_client/jito_searcher_client/__init__.py
+++ jito_searcher_client/jito_searcher_client/__init__.py
@@ -1,4 +1,4 @@
-from .async_searcher import AsyncSearcherInterceptor, get_async_searcher_client
+from .async_searcher import AsyncSearcherInterceptor, get_async_searcher_client, get_async_searcher_client_no_auth
 from .convert import tx_to_protobuf_packet
 from .generated import *
 from .searcher import SearcherInterceptor, get_searcher_client
